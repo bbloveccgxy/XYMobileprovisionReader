ba35830a7eeb5b4fb7138834e0183118fd92ec15
gaoxinyuan <294635134@qq.com>
2018-04-24 11:54:04 +0800

    init project

---
+++ XYMobileprovisionReader/XYMobileprovisionReader/XYMobileprovisionReader.h
@@ -0,0 +1,19 @@
+//
+//  XYMobileprovisionReader.h
+//  XYMobileprovisionReader
+//
+//  Created by 高昕源 on 2018/4/24.
+//  Copyright © 2018年 高昕源. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for XYMobileprovisionReader.
+FOUNDATION_EXPORT double XYMobileprovisionReaderVersionNumber;
+
+//! Project version string for XYMobileprovisionReader.
+FOUNDATION_EXPORT const unsigned char XYMobileprovisionReaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XYMobileprovisionReader/PublicHeader.h>
+
+
